62af442a9c9dab09e627b0e0344fa42ba78f81db
Miles <>
2025-03-14 09:09:10 +0800

    basic type of String

---
+++ src/main/java/com/quanqingtouru/serialize/small/core/CodecString.java
@@ -0,0 +1,42 @@
+package com.quanqingtouru.serialize.small.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static com.quanqingtouru.serialize.small.util.CodecUtil.length2Stream;
+import static com.quanqingtouru.serialize.small.util.CodecUtil.stream2Length;
+
+
+public class CodecString extends Codec<String> {
+    @Override
+    public void encode(String object, OutputStream outputStream) throws IOException {
+        if (object == null) {
+            outputStream.write(0);
+            return;
+        }
+
+        byte[] data = object.getBytes();
+        int length = data.length;
+
+        byte[] bytes = length2Stream(length);
+        outputStream.write(bytes);
+        outputStream.write(data);
+    }
+
+    @Override
+    public String decode(InputStream inputStream) throws IOException {
+        int b1 = inputStream.read();
+
+        if (((b1 >> 7) & 1) == 0) {
+            return null;
+        }
+
+        int length = stream2Length(inputStream, b1);
+
+        return new String(inputStream.readNBytes(length));
+    }
+
+}
+
+
---
+++ src/test/java/com/quanqingtouru/serialize/small/core/CodecStringTest.java
@@ -0,0 +1,30 @@
+package com.quanqingtouru.serialize.small.core;
+
+public class CodecStringTest extends CodecTestBase {
+
+    String a = null;
+    String b = "";
+    String c = "hello";
+    String d = "*".repeat(65525);
+
+    public void testA() {
+        check(a);
+    }
+
+    public void testB() {
+        check(b);
+    }
+
+    public void testC() {
+        check(c);
+    }
+
+    public void testD() {
+        check(d);
+    }
+
+    @Override
+    protected Codec<?> prepareCodec() {
+        return new CodecString();
+    }
+}
